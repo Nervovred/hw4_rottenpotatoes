f969dc719d9a02594f8cfe9384efbff3cb6a4bb2
Kirill Shchepelin <k1ll@k1ll.ru>
2012-11-06 11:51:17 +0400

    Add basic cucumber defs

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,8 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create(movie)
+  end
+  flunk "Unimplemented"
+end
